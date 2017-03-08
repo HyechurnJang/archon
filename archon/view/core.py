2dfaed61f4d80242509e5d2208593c6a7123ad53
Hye-Churn, Jang <hyjang@cisco.com>
2017-03-08 10:58:16 +0900

    Bug Fix

--- archon/view/core.py
+++ archon/view/core.py
@@ -176,14 +176,19 @@ class TABLE(TAG):
                     if isinstance(r.Length, list): r.Length = r.Length[0]
                     r.Start = int(r.Query['start'])
                     if isinstance(r.Start, list): r.Start = r.Start[0]
-                    r.OrderCol = int(r.Query['order[0][column]'])
-                    if isinstance(r.OrderCol, list): r.OrderCol = r.OrderCol[0]
-                    r.OrderDir = r.Query['order[0][dir]']
-                    if isinstance(r.OrderDir, list): r.OrderDir = r.OrderDir[0]
-                    r.Search = r.Query['search[value]']
-                    if isinstance(r.Search, list): r.Search = r.Search[0]
-                    if r.Search == '': r.Search = None
+                    
+                    try:
+                        r.OrderCol = int(r.Query['order[0][column]'])
+                        if isinstance(r.OrderCol, list): r.OrderCol = r.OrderCol[0]
+                        r.OrderDir = r.Query['order[0][dir]']
+                        if isinstance(r.OrderDir, list): r.OrderDir = r.OrderDir[0]
+                        r.Search = r.Query['search[value]']
+                        if isinstance(r.Search, list): r.Search = r.Search[0]
+                        if r.Search == '': r.Search = None
+                    except: pass
+                    
                     r.Page = r.Start / r.Length
+                    
                     return view(r, m, v)
                 
                 return decofunc
