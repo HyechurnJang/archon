a12f5f003da703f102d11202a61d529575fcc815
Hye-Churn, Jang <hyjang@cisco.com>
2017-03-08 11:21:15 +0900

    update

--- archon/wsgi.py
+++ archon/wsgi.py
@@ -46,9 +46,6 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archon.settings")
 
-import gevent.monkey
-gevent.monkey.patch_all()
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
